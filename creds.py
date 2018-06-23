28b6d40dbceb2c0e35f5c14682b4f81203c817a0
VV Labs <Vissauday@gmail.com>
2018-06-23 18:48:44 -0500

    updated creds.py

--- creds.py
+++ creds.py
@@ -1,9 +1,9 @@
 #Alexa
-Security_Profile_Description =""
-Security_Profile_ID  = ""
-Client_ID = ""
-Client_Secret = ""
-Product_ID = ""
+Security_Profile_Description ="localhosty"
+Security_Profile_ID  = "amzn1.application.2c64a1dc3ca74e6cbbe65c14455ffca8"
+Client_ID = "amzn1.application-oa2-client.6d34f9a82c56418eb68a666a3d585711"
+Client_Secret = "0a903d52f14e7ba5e45c95a3d992a76b357686aee5aaa8f8c8a38874b5e5aefe"
+Product_ID = "HerokuforApp"
 
 #Redis
 redis_url = ""
